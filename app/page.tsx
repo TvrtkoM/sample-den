eb1be8b71d4a6b6d3d0ad497bb2ad65dc6f9e546
Tvrtko Majstorovic <tvrtkomaj@gmail.com>
2025-11-29 17:47:21 +0100

    redirect from root to samples

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,5 @@
-import SamplesList from "@/components/samples/SamplesList";
-import { SamplesPageDocument } from "@/graphql-generated/graphql";
-import { defaultSamplesPageSize } from "@/lib/constants";
-import { graphqlClient } from "@/lib/graphql-client";
-import {
-  getDehydratedState,
-  getServerQueryClient
-} from "@/lib/react-query-server";
-import { HydrationBoundary } from "@tanstack/react-query";
+import { redirect } from "next/navigation";
 
 export default async function HomePage() {
-  const queryClient = getServerQueryClient();
-
-  await queryClient.prefetchQuery({
-    queryKey: ["samples", 0],
-    queryFn: () =>
-      graphqlClient.request(SamplesPageDocument, {
-        limit: defaultSamplesPageSize
-      })
-  });
-
-  const dehydrated = getDehydratedState(queryClient);
-
-  return (
-    <HydrationBoundary state={dehydrated}>
-      <SamplesList />
-    </HydrationBoundary>
-  );
+  redirect("/samples");
 }
