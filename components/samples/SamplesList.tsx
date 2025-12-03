32773e5b482c66380254ba2181d987a97eaad69d
Tvrtko Majstorovic <tvrtkomaj@gmail.com>
2025-12-03 19:23:36 +0100

    rewrite to use useSuspenseQuery

--- components/samples/SamplesList.tsx
+++ components/samples/SamplesList.tsx
@@ -3,25 +3,71 @@
 import useSamplesPage from "@/hooks/queries/useSamplesPage";
 import { defaultSamplesPageSize } from "@/lib/constants";
 import { useSamplesSearchParams } from "@/lib/search-params";
+import { useDebounce } from "@uidotdev/usehooks";
+import { Suspense } from "react";
 import AppPagination from "../AppPagination";
 import { Skeleton } from "../ui/skeleton";
 import SampleItem from "./SampleItem";
-import { useDebounce } from "@uidotdev/usehooks";
 
-const SamplesList = () => {
+const SamplesList = ({ page, search }: { page: number; search: string }) => {
+  const {
+    data: { samples }
+  } = useSamplesPage(page, search);
+
+  if (samples.length === 0) {
+    return <h1 className="container py-8 sm:py-12">No samples found.</h1>;
+  }
+  return (
+    <ul className="container py-8 sm:py-12 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6">
+      {samples.map((sample) => (
+        <SampleItem key={sample._id} sample={sample} />
+      ))}
+    </ul>
+  );
+};
+
+const SamplesSkeleton = () => {
+  return (
+    <div className="container py-8 sm:py-12 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6">
+      {Array.from({ length: defaultSamplesPageSize }, (_, i) => (
+        <Skeleton key={i} className="w-full h-64" />
+      ))}
+    </div>
+  );
+};
+
+const SamplesPagination = ({
+  page,
+  search,
+  onPageChange
+}: {
+  page: number;
+  search: string;
+  onPageChange: (page: number) => void;
+}) => {
+  const {
+    data: { totalCount }
+  } = useSamplesPage(page, search);
+
+  const totalPages =
+    totalCount === 0 ? 0 : Math.ceil(totalCount / defaultSamplesPageSize);
+
+  return (
+    <AppPagination
+      pageNum={page}
+      totalPages={totalPages}
+      onPageChange={onPageChange}
+    />
+  );
+};
+
+const SamplesListContainer = () => {
   const [searchParams, setSearchParams] = useSamplesSearchParams();
 
   const debouncedParams = useDebounce(searchParams, 300);
 
   const search = debouncedParams.search;
-  const pageNum = searchParams.page;
-
-  const { data: pageData, isFetched } = useSamplesPage(pageNum, search);
-
-  const samples = pageData?.samples ?? [];
-  const totalCount = pageData?.totalCount ?? 0;
-  const totalPages =
-    totalCount === 0 ? 0 : Math.ceil(totalCount / defaultSamplesPageSize);
+  const page = searchParams.page;
 
   const pageChangeHandler = (nextPage: number) => {
     setSearchParams({
@@ -29,30 +75,20 @@ const SamplesList = () => {
     });
   };
 
-  if (isFetched && samples.length == 0)
-    return <h1 className="container py-8 sm:py-12">No samples found.</h1>;
-
   return (
     <>
-      <ul className="container py-8 sm:py-12 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6">
-        {!isFetched
-          ? Array.from({ length: defaultSamplesPageSize }, (_, i) => (
-              <Skeleton key={i} className="w-full h-64" />
-            ))
-          : samples.map((sample) => (
-              <SampleItem key={sample._id} sample={sample} />
-            ))}
-      </ul>
-      {totalPages > 0 && (
-        <AppPagination
-          pageNum={pageNum}
-          totalPages={totalPages}
+      <Suspense fallback={<SamplesSkeleton />}>
+        <SamplesList page={page} search={search} />
+      </Suspense>
+      <Suspense>
+        <SamplesPagination
+          page={page}
+          search={search}
           onPageChange={pageChangeHandler}
-          className="container"
         />
-      )}
+      </Suspense>
     </>
   );
 };
 
-export default SamplesList;
+export default SamplesListContainer;
--- hooks/queries/useSamplesPage.ts
+++ hooks/queries/useSamplesPage.ts
@@ -1,8 +1,8 @@
 import { fetchSamplesPage } from "@/lib/fetch";
-import { useQuery } from "@tanstack/react-query";
+import { useSuspenseQuery } from "@tanstack/react-query";
 
 export default function useSamplesPage(pageNumber = 1, search = "") {
-  return useQuery({
+  return useSuspenseQuery({
     queryKey: ['samples', search, pageNumber],
     queryFn: () => {
       return fetchSamplesPage(pageNumber, search);
