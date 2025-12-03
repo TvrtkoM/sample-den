2793f139c8dc976a28a18aed42c424bc2644f664
Tvrtko Majstorovic <tvrtkomaj@gmail.com>
2025-12-03 23:40:03 +0100

    make staletime constant

--- hooks/use-samples-queries.ts
+++ hooks/use-samples-queries.ts
@@ -1,12 +1,14 @@
 import { fetchSamplesPage } from "@/lib/fetch";
 import { useSuspenseQuery } from "@tanstack/react-query";
 
+const samplePageStaleTime = 60 * 1000 * 60;
+
 export function useSuspenseSamplesPage(pageNumber = 1, search = "") {
   return useSuspenseQuery({
     queryKey: ['samples', search, pageNumber],
     queryFn: async () => {
       return fetchSamplesPage(pageNumber, search);
     },
-    staleTime: 60 * 1000 * 60
+    staleTime: samplePageStaleTime,
   })
 }
