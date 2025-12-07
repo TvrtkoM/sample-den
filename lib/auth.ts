f8a3430b13b90d742384103f7014016cc549de08
Tvrtko Majstorovic <tvrtkomaj@gmail.com>
2025-12-07 23:12:41 +0100

    remove auth plugin

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,6 @@
 import prisma from '@/lib/prisma'
 import { betterAuth } from 'better-auth'
 import { prismaAdapter } from 'better-auth/adapters/prisma'
-import { nextCookies } from 'better-auth/next-js'
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -10,7 +9,7 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true
   },
-  plugins: [nextCookies()],
+  plugins: [],
   experimental: {
     joins: true
   }
