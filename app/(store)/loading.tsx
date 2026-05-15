a799268f7f4e6728d4b2ec7a4bebe06457ef42b4
Tvrtko Majstorović <tvrtkomaj@gmail.com>
2026-05-15 23:05:44 +0200

    add loading boundaries for new layouts

---
+++ app/(store)/loading.tsx
@@ -0,0 +1,9 @@
+import { LoaderCircle } from 'lucide-react'
+
+export default function Loading() {
+  return (
+    <>
+      <LoaderCircle className="animate-spin h-10 w-10 text-primary mx-auto mt-20" />
+    </>
+  )
+}
